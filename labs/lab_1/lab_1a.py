54b98477c3e730b831b8059fa7c544241ab63194
Syam Paladugu <syampaladugu123@gmail.com>
2026-02-07 12:01:58 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,11 +3,12 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+This is to simulate change the robot speed , speed = 5 m/s
 """
 
 def main():
     print("Hello World!")
-    robotSpeed = 5
+    
     name = "Syam Paladugu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
